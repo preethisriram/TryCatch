ccee82b27fa622e046c446767e09a2ae3d6266c6
Preethi Sriram <v-psriram@microsoft.com>
2021-09-12 16:58:01 -0700

    First Commit

---
+++ FindMin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sortinganarray
+{
+    class FindMin
+    {
+
+        public void findmin(int[] a)
+        {
+            try
+            {
+                if (a.Length <= 0)
+                {
+                    throw new IndexOutOfRangeException("Probably a null array");
+                }
+
+                int small = a[0];
+                for (int i = 0; i < a.Length; i++)
+                {
+                    int curr = a[i];
+
+                    if (curr < small)
+                    {
+                        small = curr;
+                    }
+                }
+
+                Console.WriteLine(small);
+
+            }
+            
+            
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Sortinganarray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 2,19, 17, 12, 14, 16 };
+            int[] nums1 = { };
+            Console.WriteLine("The smallest element of the array is ");
+            FindMin f = new FindMin();
+           // f.findmin(nums);
+            f.findmin(nums1);
+        }
+    }
+}
